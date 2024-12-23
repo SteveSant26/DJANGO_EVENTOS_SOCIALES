885457a97067454f3c34e3dc6c30bb39e5570e0b
lesquel <holahi23holahi23@gmail.com>
2024-12-22 22:30:32 -0500

    a

--- main/views.py
+++ main/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-from negocio.models import TipoEvento
-# Create your views here.
-def home_view(request):
-    tipoEventos = TipoEvento.objects.all()[:3]
-    return render(request, 'main/home.html', {"tipoEventos": tipoEventos})
-=======
 from django.db import models
 from negocio.models import Evento
 
@@ -26,4 +19,3 @@ def home_view(request):
     return render(
         request, "main/home.html", {"eventos_mas_populares": eventos_mas_populares}
     )
->>>>>>> efe6633859bc3e8e6b75074cde5c429080a64af0
